5fa732eabcef37faf10bc8942b4cc773717b0f95
Kevin Gu <kevingu@Charless-MacBook-Pro.local>
2015-06-02 16:59:42 -0700

    added modify cells queries

--- src/helpers/Total.java
+++ src/helpers/Total.java
@@ -1,30 +1,30 @@
 package helpers;
 
 public class Total {
-	private int total;
-	private int pid;
-	private int sid;
-	public Total(int total, int pid, int sid){
+	private Integer total;
+	private Integer pid;
+	private Integer sid;
+	public Total(Integer total, Integer pid, Integer sid){
 		this.total=total;
 		this.pid=pid;
 		this.sid=sid;
 	}
-	public int getTotal() {
+	public Integer getTotal() {
 		return total;
 	}
-	public void setTotal(int total) {
+	public void setTotal(Integer total) {
 		this.total = total;
 	}
-	public int getPid() {
+	public Integer getPid() {
 		return pid;
 	}
-	public void setPid(int pid) {
+	public void setPid(Integer pid) {
 		this.pid = pid;
 	}
-	public int getSid() {
+	public Integer getSid() {
 		return sid;
 	}
-	public void setSid(int sid) {
+	public void setSid(Integer sid) {
 		this.sid = sid;
 	}
 }
